766f1b76dce16c85e1fc37c44687649d19ac8372
Prasadnair <prasadnair44@gmail.com>
2023-09-05 21:58:53 -0400

    DateOnly and TimeOnly

---
+++ DateOnlyAndTimeOnly/DateOnlyAndTimeOnly/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DateOnlyAndTimeOnly;
+
+Console.WriteLine("DateOnly Examples!");
+
+DateOnlyExamples.DateOnlyExample();
+
+DateOnlyExamples.DateOnlyFromDateTimeExample();
+
+Console.WriteLine("TimeOnly Examples!");
+
+TimeOnlyExamples.TimeOnlyExample();
+
+TimeOnlyExamples.TimeOnlyFromDateTimeExample();
+
+Console.WriteLine("Combine DateOnly and TimeOnly Examples!");
+
+DateOnly dateOnly = new DateOnly(2020, 12, 25);
+TimeOnly timeOnly = new TimeOnly(12, 30, 45);
+DateTime combinedDateTime = dateOnly.ToDateTime(timeOnly);
+
+Console.WriteLine($"Combined DateTime :  {combinedDateTime}");
+
+Console.Read();
